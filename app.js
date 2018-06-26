d3d96b46f5368c8d3feeb1df482ab7912c82e093
Kevin Potts <fancydude@Kevins-MacBook-Pro.local>
2018-06-26 13:51:49 -0600

    commit

---
+++ app.js
@@ -0,0 +1,35 @@
+let canvas = document.getElementById("pixel_canvas");
+let height = document.getElementById("input_height");
+let width = document.getElementById("input_width");
+let pickSize = document.getElementById("pickSize");
+let color = document.getElementById("colorPicker");
+
+color.addEventListener("click", function(){});
+
+pickSize.onsubmit = function(event){
+    event.preventDefault();
+    clearGrid();
+    makeGrid();
+};
+
+function makeGrid() {
+    for (let r = 0; r < height.value; r++){
+        const row = canvas.insertRow(r);
+        for (let c = 0; c < width.value; c++){
+            const cell = row.insertCell(c);
+            cell.addEventListener("click", fillSquare);
+        }
+    }
+}
+
+function clearGrid(){
+    while (canvas.firstChild){
+         canvas.removeChild(canvas.firstChild);
+    }
+}
+
+function fillSquare () {
+    this.setAttribute("style", `background-color: ${color.value}`);
+}
+
+
